7e13b9d74d30a326f0f5d3455e12d99c5104b581
You Xuye <xyouab@connect.ust.hk>
2025-12-15 17:26:34 +0800

    Add files via upload

---
+++ Arm.ino
@@ -0,0 +1,65 @@
+#include <Servo.h>
+
+Servo servo1; 
+Servo servo2; 
+Servo servo3; 
+Servo servo4;  
+Servo servo5;
+
+void setup() {
+  delay(2000); 
+  Serial.begin(921600);
+  delay(1000);
+
+  servo1.attach(16);
+  servo2.attach(5);
+  servo3.attach(4);
+  servo4.attach(0);
+  servo5.attach(14);
+
+  homePosition();
+}
+
+void loop() {
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+    Serial.print("Command: "); Serial.println(cmd);
+
+    if (cmd == "home") {
+      homePosition();
+    } else if (cmd.startsWith("s1 ")) {  //1st
+      int ang = cmd.substring(3).toInt();
+      servo1.write(constrain(ang, 0, 180));
+      Serial.println("Base -> " + String(ang) + "°");
+    } else if (cmd.startsWith("s2 ")) {  //base
+      int ang = cmd.substring(3).toInt();
+      servo2.write(constrain(ang, 0, 180));
+      Serial.println("Shoulder -> " + String(ang) + "°");
+    } else if (cmd.startsWith("s3 ")) {  //bottom 1st
+      int ang = cmd.substring(3).toInt();
+      servo3.write(constrain(ang, 0, 180));
+      Serial.println("Elbow -> " + String(ang) + "°");
+    } else if (cmd.startsWith("s4 ")) {  //middle
+      int ang = cmd.substring(3).toInt();
+      servo4.write(constrain(ang, 0, 180));
+      Serial.println("Wrist -> " + String(ang) + "°");
+    } else if (cmd.startsWith("s5 ")) {  
+      int ang = cmd.substring(3).toInt();
+      servo5.write(constrain(ang, 0, 180));
+      Serial.println("Gripper -> " + String(ang) + "°"); // gripper
+    } else {
+      Serial.println("Invaild Command");
+    }
+  }
+}
+
+void homePosition() {
+  Serial.println("Intial: 90 45 90 90 90");
+  servo1.write(90); delay(500);
+  servo2.write(45); delay(500);  // 
+  servo3.write(90); delay(500);
+  servo4.write(90); delay(500);
+  servo5.write(90); delay(500);  // 
+  Serial.println("Finish initialize");
+}
